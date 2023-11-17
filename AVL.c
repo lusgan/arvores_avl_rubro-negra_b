3e8e48e05dc50dd18fcef1bcaeca81eb93dbede5
lusgan <balbinogandolfo@gmail.com>
2023-11-17 19:53:23 -0300

    Add files via upload

--- AVL.c
+++ AVL.c
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include <time.h>
-#define arvore_quant 10000
+#define arvore_quant 1000
 
 struct Node 
 { 
@@ -151,6 +151,111 @@ int contar_elementos_no(struct Node* no) {
     return 1 + contar_elementos_no(no->left) + contar_elementos_no(no->right);
 }
 
+struct Node * minValueNode(struct Node* node)
+{
+    struct Node* current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+ 
+// Recursive function to delete a node with given key
+// from subtree with given root. It returns root of
+// the modified subtree.
+struct Node* deleteNode(struct Node* root, int key)
+{
+    // STEP 1: PERFORM STANDARD BST DELETE
+ 
+    if (root == NULL)
+        return root;
+ 
+    // If the key to be deleted is smaller than the
+    // root's key, then it lies in left subtree
+    if ( key < root->key )
+        root->left = deleteNode(root->left, key);
+ 
+    // If the key to be deleted is greater than the
+    // root's key, then it lies in right subtree
+    else if( key > root->key )
+        root->right = deleteNode(root->right, key);
+ 
+    // if key is same as root's key, then This is
+    // the node to be deleted
+    else
+    {
+        // node with only one child or no child
+        if( (root->left == NULL) || (root->right == NULL) )
+        {
+            struct Node *temp = root->left ? root->left :
+                                             root->right;
+ 
+            // No child case
+            if (temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else // One child case
+             *root = *temp; // Copy the contents of
+                            // the non-empty child
+            free(temp);
+        }
+        else
+        {
+            // node with two children: Get the inorder
+            // successor (smallest in the right subtree)
+            struct Node* temp = minValueNode(root->right);
+ 
+            // Copy the inorder successor's data to this node
+            root->key = temp->key;
+ 
+            // Delete the inorder successor
+            root->right = deleteNode(root->right, temp->key);
+        }
+    }
+ 
+    // If the tree had only one node then return
+    if (root == NULL)
+      return root;
+ 
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+    root->height = 1 + max(height(root->left),
+                           height(root->right));
+ 
+    // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to
+    // check whether this node became unbalanced)
+    int balance = getBalance(root);
+ 
+    // If this node becomes unbalanced, then there are 4 cases
+ 
+    // Left Left Case
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+ 
+    // Left Right Case
+    if (balance > 1 && getBalance(root->left) < 0)
+    {
+        root->left =  leftRotate(root->left);
+        return rightRotate(root);
+    }
+ 
+    // Right Right Case
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+ 
+    // Right Left Case
+    if (balance < -1 && getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+ 
+    return root;
+}
+
 
 int main() { 
 	struct Node *root = NULL; 
@@ -164,7 +269,10 @@ int main() {
 
 	printf("the root is %d\n",root->key);
 	printf("there are %d levels\n",root->height);
-	printf("There are %d elements\n\n",contar_elementos_no(root));
+	printf("There are %d elements\n",contar_elementos_no(root));
+	for(int i = 500; i<1000;i++){
+		root = deleteNode(root,i);
+	}
 
 	return 0; 
 } 
